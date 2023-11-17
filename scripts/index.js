58288cc9f36d1207a66400ada6c2007c64e946e1
JoaoRuffino <joaoruffino2005@gmail.com>
2023-11-17 11:31:36 -0300

    commited

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+AOS.init();
+
+class MobileNavBar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeNav = "active-nav";
+        this.activeMenu = "active-menu";
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.navList.classList.toggle(this.activeNav);
+        this.mobileMenu.classList.toggle(this.activeMenu);
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener('click', this.handleClick);
+
+        this.navLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                this.handleClick();
+            });
+        });
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavBar(
+    '.mobile-menu',
+    '.nav-list',
+    '.nav-list li'
+);
+
+mobileNavbar.init();
