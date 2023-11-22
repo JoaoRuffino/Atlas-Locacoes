61da616597c38400d3bb8c0c61b0d7343d458126
JoaoRuffino <joaoruffino2005@gmail.com>
2023-11-22 18:03:47 -0300

    Até Serviços

--- scripts/index.js
+++ scripts/index.js
@@ -39,3 +39,42 @@ const mobileNavbar = new MobileNavBar(
 );
 
 mobileNavbar.init();
+
+// Links das imagens dos Serviços
+const link_1 = document.querySelector('.link-1');
+const link_2 = document.querySelector('.link-2');
+
+link_1.addEventListener('click', () => {
+    window.location.href = "catalogo.html";
+})
+link_2.addEventListener('click', () => {
+    window.location.href = "catalogo.html";
+})
+
+// PDF.js
+
+const PDFStart = () => {
+    let loadingTask = pdfjsLib.getDocument('../assets/files/catalogo-escoramento.pdf'),
+    pdfDoc = null,
+    canvas = document.querySelector('#cnv'),
+    ctx = canvas.getContext('2d'),
+    scale = 1.5,
+    numPage = 1;
+    
+    const GeneratePDF = numPage => {
+        pdfDoc.getPage(numPage).then(page => {
+            console.log(page)
+        })
+    }
+    loadingTask.promise.then(pdfDoc_ => {
+        pdfDoc = pdfDoc_;
+        document.querySelector('#npages').textContent = pdfDoc.numPages;
+        GeneratePDF(numPage);
+    })
+}
+
+const startPDF = () => {
+    PDFStart();
+}
+
+window.addEventListener('load', startPDF);
